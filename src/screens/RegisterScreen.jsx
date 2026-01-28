7d9e4bc178f65b49d5763f14a9e924325a88f336
moragentil <moragentil@gmail.com>
2026-01-28 11:22:04 -0300

    detalles de estilos

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -43,7 +43,7 @@ function RegisterScreen({ onRegister }) {
 
   return (
     <div className="min-h-screen bg-background flex items-center justify-center px-2">
-      <div className="max-w-md w-full bg-card rounded-lg shadow-xl border border-border p-4 sm:px-8 sm:py-4">
+      <div className="max-w-md w-full bg-transparent shadow-none border-none sm:bg-card sm:rounded-lg sm:shadow-xl sm:border sm:border-border p-4 sm:px-8 sm:py-4">
         <div className="text-center mb-4">
           <img src={Logo} alt="AhorrApp Logo" className="w-36 mx-auto" />
           <h1 className="text-xl sm:text-3xl font-bold text-primary">AhorrApp</h1>
